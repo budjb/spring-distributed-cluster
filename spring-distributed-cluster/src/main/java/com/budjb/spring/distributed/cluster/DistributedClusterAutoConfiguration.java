aea8f983b8c143a206829a5a81c663497ac2a4ca
bud4453 <bud.byrd@rackspace.com>
2018-08-03 13:22:17 -0500

    Remove standalone cluster manager bean definition.

--- spring-distributed-cluster/src/main/java/com/budjb/spring/distributed/cluster/DistributedClusterAutoConfiguration.java
+++ spring-distributed-cluster/src/main/java/com/budjb/spring/distributed/cluster/DistributedClusterAutoConfiguration.java
@@ -1,18 +1,10 @@
 package com.budjb.spring.distributed.cluster;
 
-import com.budjb.spring.distributed.cluster.standalone.StandaloneClusterManager;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DistributedClusterAutoConfiguration {
-    @Bean
-    @ConditionalOnMissingBean
-    ClusterManager clusterManager(ClusterConfigurationProperties clusterConfigurationProperties) {
-        return new StandaloneClusterManager(clusterConfigurationProperties);
-    }
-
     @Bean
     ClusterConfigurationProperties clusterConfigurationProperties() {
         return new ClusterConfigurationProperties();
